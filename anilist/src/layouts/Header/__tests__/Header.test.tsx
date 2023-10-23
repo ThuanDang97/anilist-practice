96e3419efb4c6d7ce1d4c629d6136a4e8e07287c
Thuan Van Dang <thuan.dang@asnet.com.vn>
2023-10-23 17:09:59 +0700

    feat: implement unit test of header layout

---
+++ anilist/src/layouts/Header/__tests__/Header.test.tsx
@@ -0,0 +1,42 @@
+// utils
+import { render, screen } from '@utils/testUtils'
+
+// components
+import Header from '..'
+
+// mocks
+import { mockAuthNavbar, mockNavbar } from '@mocks/mockNavbar'
+
+describe('Header Layout', () => {
+  test('renders Header component with login and sign up links when not authenticated', () => {
+    render(<Header isAuthenticated={false} />)
+
+    // Assert logo is rendered
+    expect(screen.getByAltText('logo')).toBeInTheDocument()
+
+    // Assert non-authenticated Navbar links are rendered
+    mockNavbar.forEach((item) => {
+      expect(screen.getByText(item.label)).toBeInTheDocument()
+    })
+
+    // Assert Login and Sign Up links are rendered
+    expect(screen.getByText('Login')).toBeInTheDocument()
+    expect(screen.getByText('Sign Up')).toBeInTheDocument()
+  })
+
+  test('renders Header component with login and sign up links when authenticated', () => {
+    render(<Header isAuthenticated={true} />)
+
+    // Assert logo is rendered
+    expect(screen.getByAltText('logo')).toBeInTheDocument()
+
+    // Assert non-authenticated Navbar links are rendered
+    mockAuthNavbar.forEach((item) => {
+      expect(screen.getByText(item.label)).toBeInTheDocument()
+    })
+
+    // Assert Login and Sign Up links are rendered
+    expect(screen.getByText('Login')).toBeInTheDocument()
+    expect(screen.getByText('Sign Up')).toBeInTheDocument()
+  })
+})
