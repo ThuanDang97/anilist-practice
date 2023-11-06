0ed63518e2ebaa9be52f7160fc963a5d41de60b4
Thuan Van Dang <thuan.dang@asnet.com.vn>
2023-11-06 16:22:05 +0700

    refactor: remove define color of theme

--- anilist/src/themes/defaultTheme.ts
+++ anilist/src/themes/defaultTheme.ts
@@ -1,13 +1,11 @@
 import { MantineThemeOverride } from '@mantine/core'
 
-import COLORS from './color'
 import FONTS_SIZES from './fontSizes'
 
 import { Button, Badge } from './components'
 
 export const defaultTheme: MantineThemeOverride = {
   fontFamily: 'Overpass, sans-serif;',
-  colors: COLORS,
   fontSizes: FONTS_SIZES,
   components: {
     Button,
