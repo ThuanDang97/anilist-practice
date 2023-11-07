65967b992f92262c45b9f37f74a87c8497dccb0c
Thuan Van Dang <thuan.dang@asnet.com.vn>
2023-11-07 16:38:10 +0700

    feat: update UT to check toggle theme provider

--- anilist/src/__tests__/Theme.test.tsx
+++ anilist/src/__tests__/Theme.test.tsx
@@ -1,13 +1,36 @@
-import { ColorSchemeProvider } from '@mantine/core'
+import {
+  Button,
+  ColorSchemeProvider,
+  useMantineColorScheme,
+} from '@mantine/core'
 import ThemeProvider from '../theme'
 
 // utils
-import { render, screen } from '@utils/testUtils'
+import { fireEvent, render, screen } from '@utils/testUtils'
 
 describe('first', () => {
+  const Component = () => {
+    const { colorScheme, toggleColorScheme } = useMantineColorScheme()
+
+    return (
+      <div>
+        <p>{colorScheme}</p>
+        <Button
+          data-testid="toggle-button"
+          onClick={() => toggleColorScheme('dark')}
+        >
+          button
+        </Button>
+      </div>
+    )
+  }
+
   test('renders ThemeProvider component with children', () => {
     render(
-      <ColorSchemeProvider colorScheme="light" toggleColorScheme={jest.fn()}>
+      <ColorSchemeProvider
+        colorScheme="light"
+        toggleColorScheme={jest.fn(() => 'dark')}
+      >
         <ThemeProvider>
           <div>Child Component</div>
         </ThemeProvider>
@@ -16,5 +39,36 @@ describe('first', () => {
 
     // Assert that child component is rendered
     expect(screen.getByText('Child Component')).toBeInTheDocument()
+  })
+
+  test('toggleColorScheme function toggles the color scheme to dark correctly', () => {
+    render(
+      <ColorSchemeProvider colorScheme="light" toggleColorScheme={() => {}}>
+        <ThemeProvider>
+          <Component />
+        </ThemeProvider>
+      </ColorSchemeProvider>,
+    )
+
+    const toggleButton = screen.getByTestId('toggle-button')
+    fireEvent.click(toggleButton)
+
+    expect(screen.getByText('dark')).toBeInTheDocument()
+  })
+
+  test('toggleColorScheme function toggles the color scheme to light correctly', () => {
+    render(
+      <ColorSchemeProvider colorScheme="dark" toggleColorScheme={() => {}}>
+        <ThemeProvider>
+          <Component />
+        </ThemeProvider>
+      </ColorSchemeProvider>,
+    )
+
+    const toggleButton = screen.getByTestId('toggle-button')
+    fireEvent.click(toggleButton)
+    fireEvent.click(toggleButton)
+
+    expect(screen.getByText('light')).toBeInTheDocument()
   })
 })
