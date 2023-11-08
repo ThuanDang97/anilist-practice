2a15e4c61f0c44fbc2ef36a0cc286b3550a490b9
Thuan Van Dang <thuan.dang@asnet.com.vn>
2023-11-08 14:29:38 +0700

    feat: create ui list button toggle theme

--- anilist/src/layouts/Footer/index.tsx
+++ anilist/src/layouts/Footer/index.tsx
@@ -1,9 +1,7 @@
-import { Box, List, Popover, Text } from '@mantine/core'
-import { useDisclosure } from '@mantine/hooks'
+import { Box, Button, Group, HoverCard, List, Text } from '@mantine/core'
 import { Link } from 'react-router-dom'
 
-// styles
-import { useStylesFooter } from './Footer.module'
+// mocks
 import {
   mockAbout,
   mockConnect,
@@ -11,18 +9,35 @@ import {
   mockSupport,
 } from '@mocks/mockNavFooter'
 
+// styles
+import { useStylesFooter } from './Footer.module'
+
 type ListLink = {
   label: string
   link: string
 }
 
+const listSiteTheme = [
+  {
+    type: 'light',
+    desc: 'Light',
+  },
+  {
+    type: 'dark',
+    desc: 'Dark',
+  },
+  {
+    type: 'default',
+    desc: 'System Theme',
+  },
+]
+
 const Footer = () => {
   const { classes } = useStylesFooter()
-  const [opened, { close, open }] = useDisclosure(false)
 
   const renderList = (listLink: ListLink[]) => {
     return (
-      <List className={classes.list}>
+      <List>
         {listLink.map((item) => (
           <List.Item key={item.label} className={classes.link}>
             <Link to={item.link} role="menuitem">
@@ -37,25 +52,73 @@ const Footer = () => {
   return (
     <Box component="footer" className={classes.footer} role="contentinfo">
       <Box className={classes.container}>
-        <Box className={classes.themes}>
+        <Box>
           <Text className={classes.themeText}>Site Theme</Text>
-          <Popover
-            width={100}
-            position="top-start"
-            withArrow
-            shadow="md"
-            opened={opened}
-            aria-label="Select Site Theme"
-          >
-            <Popover.Target>
-              <Text onMouseEnter={open} onMouseLeave={close}>
-                Hover
-              </Text>
-            </Popover.Target>
-            <Popover.Dropdown style={{ pointerEvents: 'none' }}>
-              <Text size="sm">Light</Text>
-            </Popover.Dropdown>
-          </Popover>
+          <Group>
+            {listSiteTheme.map((theme) => (
+              <HoverCard
+                position="top-start"
+                withArrow
+                shadow="xs"
+                aria-label="Select Site Theme"
+                key={theme.type}
+                variant="dark"
+                closeDelay={100}
+                styles={{
+                  dropdown: {
+                    width: 'fit-content',
+                    padding: '3px',
+                    textAlign: 'center',
+                    background: '#303133',
+                    color: '#fff',
+                    borderColor: '#303133',
+                  },
+                  arrow: {
+                    borderColor: '#303133',
+                  },
+                }}
+              >
+                <HoverCard.Target>
+                  <Button
+                    sx={{
+                      background:
+                        theme.type === 'dark'
+                          ? '#11161d'
+                          : theme.type === 'light'
+                          ? '#edf1f5'
+                          : 'linear-gradient(45deg,#11161d 55%,#edf1f5 0)',
+                      color:
+                        theme.type === 'dark'
+                          ? '#edf1f5'
+                          : theme.type === 'light'
+                          ? '#11161d'
+                          : 'linear-gradient(45deg,#11161d 55%,#edf1f5 0)',
+                      ':hover': {
+                        background:
+                          theme.type === 'dark'
+                            ? '#11161d'
+                            : theme.type === 'light'
+                            ? '#edf1f5'
+                            : 'linear-gradient(45deg,#11161d 55%,#edf1f5 0)',
+                        color:
+                          theme.type === 'dark'
+                            ? '#edf1f5'
+                            : theme.type === 'light'
+                            ? '#11161d'
+                            : 'linear-gradient(45deg,#11161d 55%,#edf1f5 0)',
+                      },
+                    }}
+                    className={classes.button}
+                  >
+                    A
+                  </Button>
+                </HoverCard.Target>
+                <HoverCard.Dropdown>
+                  <Text size={'xs'}>{theme.desc}</Text>
+                </HoverCard.Dropdown>
+              </HoverCard>
+            ))}
+          </Group>
         </Box>
         <Box
           className={classes.nav}
