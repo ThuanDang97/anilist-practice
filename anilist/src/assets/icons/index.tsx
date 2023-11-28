bf40f9210a959a576b1cc14971be56810951e9e4
Thuan Van Dang <thuan.dang@asnet.com.vn>
2023-11-28 17:45:03 +0700

    refactor: import icon

---
+++ anilist/src/assets/icons/index.tsx
@@ -0,0 +1,17 @@
+import AppStoreIcon from './AppStoreIcon'
+import DiscordIcon from './DiscordIcon'
+import EmailIcon from './EmailIcon'
+import HeartIcon from './HeartIcon'
+import SettingIcon from './SettingIcon'
+import SignOutIcon from './SignOutIcon'
+import UserIcon from './UserIcon'
+
+export {
+  AppStoreIcon,
+  DiscordIcon,
+  EmailIcon,
+  HeartIcon,
+  SettingIcon,
+  SignOutIcon,
+  UserIcon,
+}
