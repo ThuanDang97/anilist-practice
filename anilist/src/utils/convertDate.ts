f7670a6e8b70b874b290444836a6661a24d7d709
Thuan Van Dang <thuan.dang@asnet.com.vn>
2023-11-30 17:30:09 +0700

    refactor: convert date time

--- anilist/src/utils/convertDate.ts
+++ anilist/src/utils/convertDate.ts
@@ -5,5 +5,22 @@ interface DateObj {
 }
 
 export const ConvertDateTime = (date: DateObj): string => {
-  return new Date(date.year, date.month - 1, date.day, 0, 0, 0, 0).toISOString()
+  // Create a Date object, set its local time to midnight
+  const localMidnightDate = new Date(
+    date.year,
+    date.month - 1,
+    date.day,
+    0,
+    0,
+    0,
+    0,
+  )
+
+  localMidnightDate.setDate(localMidnightDate.getDate())
+
+  const utcDate = new Date(
+    localMidnightDate.getTime() - localMidnightDate.getTimezoneOffset() * 60000,
+  )
+
+  return utcDate.toISOString()
 }
