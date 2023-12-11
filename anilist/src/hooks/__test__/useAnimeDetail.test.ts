06933ee28b7b6a862f01c7a81a07073b38ace1b3
Thuan Van Dang <thuan.dang@asnet.com.vn>
2023-12-11 14:13:09 +0700

    feat: create test hook useAnimeDetail

---
+++ anilist/src/hooks/__test__/useAnimeDetail.test.ts
@@ -0,0 +1,35 @@
+import { renderHook, waitFor } from '@testing-library/react'
+
+// hooks
+import useAnimeDetail from '@hooks/useAnime/useAnimeDetail'
+
+// mocks
+import { mockAnime } from '@mocks/mockAnime'
+
+//utils
+import { wrapper } from '@utils/testUtils'
+
+import * as services from '@services/animeService'
+
+jest.mock('@services/animeService', () => ({
+  __esModule: true,
+  ...jest.requireActual('@services/animeService'),
+}))
+
+describe('useAnimeDetail', () => {
+  test('useAnimeDetail', async () => {
+    jest.spyOn(services, 'getDetailAnime').mockResolvedValue(mockAnime[0])
+    const { result } = renderHook(() => useAnimeDetail('1234'), {
+      wrapper,
+    })
+
+    await waitFor(() => {
+      expect(result.current.isSuccess).toBe(true)
+    })
+
+    await waitFor(() => {
+      console.log('result', result.current.data)
+      expect(result.current.data).toEqual(mockAnime[0])
+    })
+  })
+})
